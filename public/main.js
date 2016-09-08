b1e13effcbc1ff7df6700e847ce6439952e7683e
Daniela Noir <daniela@danielanoir.com>
2016-09-07 21:45:59 -0400

    scrolling to correct places again. Removed half arrows because they wouldn't stick to the bottom of the div.

--- public/main.js
+++ public/main.js
@@ -4,19 +4,31 @@ $(document).ready(function(){
   var hideWelcome = function() {
     $(".welcomeText").hide();
   }
-  var hideSkills = function() {
-    $(".skillsPopOut").hide();
-  }
+
   $(function(){
     $(document).scroll(function() {
       hideWelcome();
-      if($(document).scrollTop() == 0)
+      if($(document).scrollTop() >= 0 && $(document).scrollTop() <=800)
       $(".welcomeText").fadeIn("slow", function() {
       })
     })
   })
 
+  $(window).scroll(function(){
+    if($(this).scrollTop() > 1200) {
+      $('.skillsPopOut').slideDown("slow");
+    } else {
+      $('.skillsPopOut').slideUp('slow');
+    }
+  })
 
+  $(window).scroll(function(){
+    if($(this).scrollTop() > 600) {
+      $('.aboutPopOut').slideDown("slow");
+    } else {
+      $('.aboutPopOut').slideUp('slow');
+    }
+  })
 
   $("#welcome").click(function() {
     var offset = $(this).offset();
@@ -24,27 +36,27 @@ $(document).ready(function(){
   });
   $(".roundArrow").click(function() {
     var offset = $(this).offset();
-    $('html, body').animate({scrollTop: 600}, 400);
+    $('html, body').animate({scrollTop: 950}, 400);
   });
   $("#about").click(function() {
     var offset = $(this).offset();
-    $('html, body').animate({scrollTop: 600}, 400);
+    $('html, body').animate({scrollTop: 950}, 400);
   });
-  $(".aboutPageArrow").click(function() {
-    var offset = $(this).offset();
-    $('html, body').animate({scrollTop: 1500}, 400);
-  });
+  // $(".aboutPageArrow").click(function() {
+  //   var offset = $(this).offset();
+  //   $('html, body').animate({scrollTop: 2300}, 400);
+  // });
   $("#skills").click(function() {
     var offset = $(this).offset();
-    $('html, body').animate({scrollTop: 1500}, 400);
+    $('html, body').animate({scrollTop: 1850}, 400);
   });
-  $(".skillsPageArrow").click(function() {
-    var offset = $(this).offset();
-    $('html, body').animate({scrollTop: 2400}, 400);
-  });
+  // $(".skillsPageArrow").click(function() {
+  //   var offset = $(this).offset();
+  //   $('html, body').animate({scrollTop: 2400}, 400);
+  // });
   $("#projects").click(function() {
     var offset = $(this).offset();
-    $('html, body').animate({scrollTop: 2400}, 400);
+    $('html, body').animate({scrollTop: 2950}, 400);
   });
   $("#contact").click(function() {
     var offset = $(this).offset();
