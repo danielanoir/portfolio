05d9fdab820249aa29beaa6450db6eeb91283b77
Daniela Noir <daniela@danielanoir.com>
2016-09-09 08:18:25 -0400

    first commit. coppied from other repo

--- public/main.js
+++ public/main.js
@@ -5,30 +5,20 @@ $(document).ready(function(){
     $(".welcomeText").hide();
   }
 
-  $(function(){
-    $(document).scroll(function() {
-      hideWelcome();
-      if($(document).scrollTop() >= 0 && $(document).scrollTop() <=800)
-      $(".welcomeText").fadeIn("slow", function() {
-      })
-    })
-  })
-
-  $(window).scroll(function(){
-    if($(this).scrollTop() > 1200) {
-      $('.skillsPopOut').slideDown("slow");
-    } else {
-      $('.skillsPopOut').slideUp('slow');
-    }
-  })
-
-  $(window).scroll(function(){
-    if($(this).scrollTop() > 600) {
-      $('.aboutPopOut').slideDown("slow");
-    } else {
-      $('.aboutPopOut').slideUp('slow');
-    }
-  })
+  // $(window).scroll(function(){
+  //   if($(this).scrollTop() > 600) {
+  //     $('.aboutPopOut').slideDown("slow");
+  //   } else {
+  //     $('.aboutPopOut').slideUp('slow');
+  //   }
+  // })
+  // $(window).scroll(function(){
+  //   if($(this).scrollTop() > 1200) {
+  //     $('.skillsPopOut').slideDown("slow");
+  //   } else {
+  //     $('.skillsPopOut').slideUp('slow');
+  //   }
+  // })
 
   $("#welcome").click(function() {
     var offset = $(this).offset();
@@ -36,27 +26,31 @@ $(document).ready(function(){
   });
   $(".roundArrow").click(function() {
     var offset = $(this).offset();
-    $('html, body').animate({scrollTop: 950}, 400);
+    $('html, body').animate({scrollTop: 830}, 400);
   });
   $("#about").click(function() {
     var offset = $(this).offset();
-    $('html, body').animate({scrollTop: 950}, 400);
+    $('html, body').animate({scrollTop: 830}, 400);
   });
-  // $(".aboutPageArrow").click(function() {
-  //   var offset = $(this).offset();
-  //   $('html, body').animate({scrollTop: 2300}, 400);
-  // });
+    $(".aboutArrowDiv").click(function() {
+      var offset = $(this).offset();
+      $('html, body').animate({scrollTop: 1750}, 400);
+    });
   $("#skills").click(function() {
     var offset = $(this).offset();
-    $('html, body').animate({scrollTop: 1850}, 400);
+    $('html, body').animate({scrollTop: 1750}, 400);
   });
-  // $(".skillsPageArrow").click(function() {
-  //   var offset = $(this).offset();
-  //   $('html, body').animate({scrollTop: 2400}, 400);
-  // });
+  $(".skillsArrowDiv").click(function() {
+    var offset = $(this).offset();
+    $('html, body').animate({scrollTop: 2750}, 400);
+  });
   $("#projects").click(function() {
     var offset = $(this).offset();
-    $('html, body').animate({scrollTop: 2950}, 400);
+    $('html, body').animate({scrollTop: 2750}, 400);
+  });
+  $(".projectsArrowDiv").click(function() {
+    var offset = $(this).offset();
+    $('html, body').animate({scrollTop: 4000}, 400);
   });
   $("#contact").click(function() {
     var offset = $(this).offset();
